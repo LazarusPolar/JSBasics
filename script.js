e7d88dd22ea72d622c5b87e8a0f9cd7bed9f8652
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-06-12 13:32:56 -0500

    Agrega ejemplo de tipos de datos

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world!")
+// Prim. Datatypes:
+// Number, String, Boolean, Undefined, Null
+
+var name = 'Jose';
+console.log(name);
+
+var lastName = 'Almaraz';
+console.log(lastName);
+
+var age = 24;
+console.log(age);
