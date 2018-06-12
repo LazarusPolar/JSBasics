e557cf2b5e1af2e6d8a8c57a0acdc27390ed451b
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-06-12 17:37:38 -0500

    Agrega ejemplo de variables y operadores

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Prim. Datatypes:
 // Number, String, Boolean, Undefined, Null
 
-var name = 'Jose';
+/*var name = 'Jose';
 console.log(name);
 
 var lastName = 'Almaraz';
@@ -9,3 +9,53 @@ console.log(lastName);
 
 var age = 24;
 console.log(age);
+
+var fullAge = true;
+console.log(fullAge);*/
+
+// Lecture: Var 2
+/*
+var name = "Jose";
+var age  = 24;
+
+console.log(name + age);
+console.log(age + age);
+
+var job, isMarried;
+console.log(job);
+
+job = "Engineer";
+isMarried = false;
+console.log(job);
+console.log(isMarried);
+
+console.log(name + ' is ' + age + ' years old ' + job + ' Is he married? ' + isMarried);
+
+age = 'Thirty Six'
+job = 'Teacher'
+
+console.log(name + ' is ' + age + ' years old ' + job + ' Is he married? ' + isMarried);
+
+// To call for an input on JS
+var lastName = prompt('Whats your last name');
+console.log(lastName);
+
+alert(name + ' is ' + age + ' years old ' + job + ' Is he married? ' + isMarried);*/
+
+// Lecture: Operators
+var now = 2018;
+var birthYear = now - 24;
+birthYear = now - 26 * 2;
+
+console.log(birthYear);
+
+var ageJohn = 20; 
+var ageMark = 30;
+
+ageJohn = ageMark = 3 + 5 * 4 - 6;
+
+ageMark *= 2;
+ageJohn++;
+
+console.log(ageJohn);
+console.log(ageMark);
